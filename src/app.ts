1c3cd8b779d8bb8fa68e8fbf54d62e6b27eebc52
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-21 03:50:44 +0300

    add func countResult

--- src/app.ts
+++ src/app.ts
@@ -9,20 +9,24 @@ import * as c from './consts';
 
 let players: IPlayer[] = [];
 let arrIssues: any;
+let arrCards: any[];
 let gameSettings: any;
+let counterReady = false;
 const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({ server });
 
+const results = new Map();
 const sockets = new Map();
+const statistic = new Map();
 
 function sendDataToPlayers(data: any) {
-  sockets.forEach((socket) => {
+  if (sockets.size) sockets.forEach((socket) => {
     socket.send(JSON.stringify(data));
   });
 }
 function closeSockets() {
-  sockets.forEach((socket) => {
+  if (sockets.size) sockets.forEach((socket) => {
     socket.close(1000, 'Scrum master leaved this session...');
   });
 }
@@ -32,22 +36,39 @@ function deletePlayer(id: number, mes: string) {
   if (sockets.has(id)) sockets.get(id).close(1000, mes);
   sendDataToPlayers({ type: c.SET_PLAYERS, players });
 }
+async function countResult(issue: number): Promise<string[] | undefined> {
+  if(!counterReady) {
+    counterReady = true;
+    const arr = Object.values(results.get(issue));
+    const resultArr = [];
+    for (let i = 0; i < arrCards.length; i += 1) {
+      resultArr.push((arr.filter((item) => item === i).length/arr.length).toFixed(2))
+    }
+    statistic.set(issue, resultArr);
+    return resultArr
+  }
+  return undefined;
+}
 
 webSocketServer.on('connection', (ws) => {
   ws.on('message', (m: string): void => {
-    const { type, player, id, location, idSession, issues, settings }: { type: string, player: IPlayer, id: number, location: string, idSession: string, issues: any, settings: any } =
+    const { type, player, playerId, location, idSession, issues, settings, issue, card }: { type: string, player: IPlayer, playerId: number, location: string, idSession: string, issues: any, settings: any, issue: number, card: number } =
       JSON.parse(m);
     switch (type) {
       case c.SET_SESSION:
-        console.dir('SET_SESSION')
         if (sockets.size !== 0) {
           closeSockets();
           sockets.clear();
+          results.clear();
+          statistic.clear();
+
           players = [];
+          arrIssues = [];
+          arrCards = [];
         }
         break;
       case c.CHECK_ID_SESSION:
-        if(!players.length || (players.length && idSession !== String(players[0]?.id))) {console.dir(players.length); ws.close(1000, ' Access denied... ');}
+        if(!players.length || (players.length && idSession !== String(players[0]?.id))) ws.close(1000, ' Access denied... ');
         break;
       case c.PUT_PLAYER:
           if (!players.length) players[0] = player;
@@ -56,21 +77,49 @@ webSocketServer.on('connection', (ws) => {
           sendDataToPlayers({ type: c.SET_PLAYERS, players });
         break;
       case c.DEL_PLAYER:
-        if (sockets.get(players[0]?.id) === ws) deletePlayer(id, 'scrum master deleted you from session');
+        if (sockets.get(players[0]?.id) === ws) deletePlayer(playerId, 'scrum master deleted you from session');
         break;
       case c.SET_LOCATION:
         ws.send(JSON.stringify({ type: c.SET_LOCATION, location }));
         break;
       case c.CLOSE_SESSION:
-        if( id === players[0].id) closeSockets()
-        else deletePlayer(id, 'Session closed...');
+        if( playerId === players[0].id) closeSockets()
+        else deletePlayer(playerId, 'Session closed...');
         break;
       case c.START_GAME:
-        arrIssues = issues;
-        gameSettings = settings;
+        // arrIssues = issues;
+        // gameSettings = settings;
+        arrCards = settings.cardStorage;
+        sendDataToPlayers( {type: c.SET_SETTINGS, issues, settings});
         sendDataToPlayers({ type: c.SET_LOCATION, location: '/game' });
-        console.dir(arrIssues)
-        console.dir(gameSettings)
+        break;
+      case c.SET_ROUND_START:
+        results.set(issue, {})
+        // results.set(issue, [])
+        sendDataToPlayers( {type: c.SET_ROUND_START, issue})
+        // sendDataToPlayers({ type: c.SET_LOCATION, location: '/game' });
+        console.dir('SET_ROUND_START')
+        break;
+      case c.SET_ROUND_RESULT:
+        console.dir('SET_ROUND_RESULT')
+        if (Object.keys(results.get(issue)).length) setTimeout(countResult, 10000)
+        results.set(issue, {...results.get(issue), [playerId]: card})
+        // results.set(issue, [...results.get(issue), {[playerId]: card}])
+        // if (sockets.size === results.get(issue).length && !counterReady) countResult(issue)
+        if (sockets.size === Object.keys(results.get(issue)).length && !counterReady) {
+          countResult(issue).
+          then((res) => {
+            if(res) {console.dir(res)
+              // sendDataToPlayers({ type: c.SET_LOCATION, location: '/game' });
+            }
+          })
+        }
+        // arrIssues = issues;
+        // gameSettings = settings;
+        // sendDataToPlayers( {type: c.SET_SETTINGS, issues, settings})
+        // sendDataToPlayers({ type: c.SET_LOCATION, location: '/game' });
+        
+
         break;
       default: break;
     }
--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,6 @@ export const SET_SESSION = 'SET_SESSION';
 export const CHECK_ID_SESSION = 'CHECK_ID_SESSION';
 export const CLOSE_SESSION = 'CLOSE_SESSION';
 export const START_GAME = 'START_GAME';
+export const SET_SETTINGS = 'SET_SETTINGS';
+export const SET_ROUND_RESULT = 'SET_ROUND_RESULT';
+export const SET_ROUND_START = 'SET_ROUND_START';
